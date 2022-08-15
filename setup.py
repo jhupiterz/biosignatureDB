4c4c6e52e00bbad4847d549a9e40bec1e1a2a6e2
Julie Hartz <julie.hartz13@gmail.com>
2022-08-15 17:39:44 +0200

    initial push to heroku

--- setup.py
+++ setup.py
@@ -13,5 +13,4 @@ setup(name='biosignature_db',
       test_suite='tests',
       # include_package_data: to install data from MANIFEST.in
       include_package_data=True,
-      scripts=['scripts/biosignature_db-run'],
       zip_safe=False)
